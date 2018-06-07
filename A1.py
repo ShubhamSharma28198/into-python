0e928b2a44d68c546f077cede834a880c97fce64
ShubhamSharma28198 <39906233+ShubhamSharma28198@users.noreply.github.com>
2018-06-07 10:20:58 +0530

    Add files via upload
    done

---
+++ A1.py
@@ -0,0 +1,32 @@
+#Ques1-->
+#Print anything ypu want on screen
+print("python classes")
+
+#Ques2-->
+#Join 2 string
+print("Acad"+"view")
+
+#Ques3-->
+#print the values of 3 variables x,y,z
+x=input("enter first no:")
+y=input("enter 2nd no \t")
+z=input("enter 3rd no\t")
+print(x,y,z)
+
+#Ques4-->
+# print"let's get screen" on the screen
+print("Let's get started")
+
+#Ques5-->
+#print vlaues using placeholder
+s="Acadview"
+course="python"
+fees=5000
+print(s,course,fees,sep='--')
+
+#Ques6-->
+#Let's do some" ineresting exercise use fill in the blanks
+name="Shubham"
+salary=500000
+print("%s has %d salary" %(name,salary))
+
